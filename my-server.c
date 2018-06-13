c7dd1d6354abe788a14d56f25d7711bc8097eca3
viduni94 <viduni.ushanka@gmail.com>
2018-06-13 21:13:13 +0530

    change location of main function

--- my-server.c
+++ my-server.c
@@ -25,7 +25,7 @@ Instructions: The port number should be passed as an argument
 #define EOL "\r\n"
 #define EOL_SIZE 2
 
-#define BACKLOG 5 //The number of pending connections the queue will hold
+#define BACKLOG 10 //The number of pending connections the queue will hold
 
 //Structure for extensions and media types
 typedef struct {
@@ -42,68 +42,10 @@ int get_file_size(int fd);
 void php_cgi(char* script_path, int fd);
 void* get_in_addr (struct sockaddr* sa);
 
-int main(int argc, char *argv[]) {
-
-	int sockfd, newsockfd, portno, pid;
-	socklen_t clientlen;
-	struct sockaddr_in server_addr, client_addr;
-	char s[INET6_ADDRSTRLEN];
-
-	//Check whether the port is passed as an argument
-	if(argc < 2) {
-		fprintf(stderr, "ERROR, no port provided\n");
-		exit(1);
-	}
-
-	//socket creation
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);  //IPv4 domain, TCP (sock_stream), Protocol value for IP
-	if(sockfd < 0)
-		perror("Error in opening socket");
-	bzero((char *) &server_addr, sizeof(server_addr));  //Copies zeroes to the size of the server address to the server address buffer
-	portno = atoi(argv[1]);  //Get the port number from the argument, atoi converts string argument to integer
-
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = INADDR_ANY;
-	server_addr.sin_port = htons(portno);  //htons converts unsigned short integer hostshort from host byte order to network byte order
-
-	//Bind the socket to the address and the port number
-	if(bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0 )
-		perror("Error in binding");
-	//Listening to any client requests with a maximum of 5 connections in the queue
-	listen(sockfd, BACKLOG);
-	clientlen = sizeof(client_addr);
-
-	//Makes the server run continuously and create a seperate process for each connection
-	while(1) {
-		newsockfd = accept(sockfd, (struct sockaddr *) &client_addr, &clientlen);
-		if(newsockfd < 0)
-			perror("Error in accepting");
-		pid = fork();
-		if(pid < 0)
-			perror("Error in fork");
-		if(pid == 0) { //child process
-			close(sockfd);
-			//Process request
-			process_request(newsockfd);
-
-			//Print message to show that a connection was received (inet_ntop converts IP to human readable format, get_in_addr gets IP address)
-			inet_ntop(client_addr.sin_family, get_in_addr((struct sockaddr *) &client_addr), s, sizeof s);
-			printf("Server: Received a connection from %s\n", s);
-
-			exit(0);
-		} else {
-			close(newsockfd);
-		}
-	} //end of while loop
-
-	close(sockfd);
-	return 0; //Unreachable code
-
-}
-
 //Possible media types
 extn extensions[] ={
 	{"html","text/html" },
+	{"htm","text/html" },
 	{"php", "text/html" },
 	{"txt", "text/plain" },
 	{"jpg", "image/jpg" },
@@ -121,7 +63,7 @@ int process_request(int fd) {
 	char request[500], resource[500], *pointer;
 	int fd1, length;
 
-	//Check if requesrt is received
+	//Check if request is received
 	if(receive_new(fd, request) == 0) {
 		printf("Request Receiving Failed\n");
 	}
@@ -144,7 +86,9 @@ int process_request(int fd) {
 			printf("Unknown Request\n");
 		} else {
 			if(pointer[strlen(pointer) - 1] == '/') {
-				strcat(pointer, "index.html");
+				//appends viduni.html to pointer
+				printf("121");
+				strcat(pointer, "viduni.html");
 			}
 			strcpy(resource, webroot());
 			strcat(resource, pointer);
@@ -161,9 +105,11 @@ int process_request(int fd) {
 					if(fd1 == -1) {
 						printf("404 file not found error\n");
 						send_response(fd, "HTTP/1.1 404 Not Found\r\n");
-						send_response(fd, "Server : Web Server in C\r\n\r\n");
-						send_response(fd, "<html><head><title>404 Not Found</title></head>");
-						send_response(fd, "<body><p>404 Not Found: The requested resource could not be found!</p></body></html>");
+						send_response(fd, "Server : Web Server in C\r\n");
+						send_response(fd, "Content-Type: text/html; charset=utf-8\r\n");
+						send_response(fd, "<html><head><title>404 Not Found</title></head>\r\n");
+						send_response(fd, "<body><p>404 Not Found: The requested resource could not be found!</p></body></html>\r\n\r\n");
+
 					//Handling php requests
 					} else if(strcmp(extensions[i].ext, "php") == 0) {
 						//printf("%s\n", extensions[i].ext);
@@ -175,7 +121,10 @@ int process_request(int fd) {
 						printf("200 OK, Content-Type: %s\n\n", extensions[i].mediatype);
 						send_response(fd, "HTTP/1.1 200 OK\r\n");
 						send_response(fd, "Server :  Web Server in C\r\n");
-						send_response(fd, "Content-Type: text/html; charset=utf-8\r\n\r\n");
+						send_response(fd, "Content-Type: text/html; charset=utf-8\r\n");
+						send_response(fd, "Content-Length: 348000\r\n");
+						send_response(fd, "Connection: keep-alive, upgrade\r\n");
+						send_response(fd, "Keep-Alive: timeout=5, max=1000\r\n\r\n");
 
 						//If the request is a GET request
 						if(pointer == request + 4) {
@@ -206,9 +155,9 @@ int process_request(int fd) {
 				if(i == size-1) {
 					printf("415 Unsupported Media Type\n");
 			        send_response(fd, "HTTP/1.1 415 Unsupported Media Type\r\n");
-			        send_response(fd, "Server : Web Server in C\r\n\r\n");
-			        send_response(fd, "<html><head><title>415 Unsupported Media Type</head></title>");
-			        send_response(fd, "<body><p>415 Unsupported Media Type!</p></body></html>");
+			        send_response(fd, "Server : Web Server in C\r\n");
+			        send_response(fd, "<html><head><title>415 Unsupported Media Type</head></title>\r\n");
+			        send_response(fd, "<body><p>415 Unsupported Media Type!</p></body></html>\r\n\r\n");
 				}
 			}
 			close(fd);
@@ -218,7 +167,7 @@ int process_request(int fd) {
 	/* Makes the full-duplex connection on the socket associated with the
 	file descriptor socket to be shut down */
 	shutdown(fd, SHUT_RDWR);
-	return(0);
+	//return(0);
 }
 
 //Function to receive the buffer until EOL is reached
@@ -309,6 +258,65 @@ void* get_in_addr (struct sockaddr* sa) {
   } else {
     return &(((struct sockaddr_in6*) sa)->sin6_addr);
   }
+}
+
+int main(int argc, char *argv[]) {
+
+	int sockfd, newsockfd, portno, pid;
+	socklen_t clientlen;
+	struct sockaddr_in server_addr, client_addr;
+	char s[INET6_ADDRSTRLEN];
+
+	//Check whether the port is passed as an argument
+	if(argc < 2) {
+		fprintf(stderr, "ERROR, no port provided\n");
+		exit(1);
+	}
+
+	//socket creation
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);  //IPv4 domain, TCP (sock_stream), Protocol value for IP
+	if(sockfd < 0)
+		perror("Error in opening socket");
+	bzero((char *) &server_addr, sizeof(server_addr));  //Copies zeroes to the size of the server address to the server address buffer
+	portno = atoi(argv[1]);  //Get the port number from the argument, atoi converts string argument to integer
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_port = htons(portno);  //htons converts unsigned short integer hostshort from host byte order to network byte order
+
+	//Bind the socket to the address and the port number
+	if(bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0 )
+		perror("Error in binding");
+	//Listening to any client requests with a maximum of 10 connections in the queue
+	listen(sockfd, BACKLOG);
+	clientlen = sizeof(client_addr);
+
+	//Makes the server run continuously and create a seperate process for each connection
+	while(1) {
+		newsockfd = accept(sockfd, (struct sockaddr *) &client_addr, &clientlen);
+		if(newsockfd < 0)
+			perror("Error in accepting");
+		pid = fork();
+		if(pid < 0)
+			perror("Error in fork");
+		if(pid == 0) { //child process
+			close(sockfd);
+			//Process request
+			process_request(newsockfd);
+
+			//Print message to show that a connection was received (inet_ntop converts IP to human readable format, get_in_addr gets IP address)
+			inet_ntop(client_addr.sin_family, get_in_addr((struct sockaddr *) &client_addr), s, sizeof s);
+			printf("Server: Received a connection from %s\n", s);
+
+			exit(0);
+		} else {
+			close(newsockfd);
+		}
+	} //end of while loop
+
+	close(sockfd);
+	return 0; //Unreachable code
+
 }
 
 
